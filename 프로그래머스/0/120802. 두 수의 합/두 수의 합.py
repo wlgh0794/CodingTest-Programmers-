a74b0adb80513a2564138f296c2aab7ec236788b
wlgh0794 <167949216+wlgh0794@users.noreply.github.com>
2024-06-12 19:33:56 +0900

    [level 0] Title: 두 수의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.py
@@ -0,0 +1,18 @@
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+
+
+if __name__ == "__main__" : 
+    
+    
+    Data_list1 = [ 2 , 100 ] 
+    Data_list2 = [ 3 , 2 ]
+    
+    
+    for i in Data_list1 :
+    
+        for k in Data_list2 :
+            
+            solution( i , k )
