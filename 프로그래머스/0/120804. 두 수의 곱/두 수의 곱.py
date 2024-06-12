5b2a3341a82f725e096cd0ca78be8c4c36d0769d
wlgh0794 <167949216+wlgh0794@users.noreply.github.com>
2024-06-12 19:27:30 +0900

    [level 0] Title: 두 수의 곱, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.py
@@ -0,0 +1,17 @@
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+
+
+if __name__ == "__main__" :
+    
+    Data_list1 = [ 3 , 27 ]
+
+    Data_list2 = [ 4 , 19 ]
+    
+    for i in Data_list1 :
+        
+        for i in Data_list2 :
+        
+            solution( Data_list1 , Data_list2  )
