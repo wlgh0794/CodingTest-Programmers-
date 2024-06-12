8cd9f67785e7ca16b9578bcf5583c93ead0977af
wlgh0794 <167949216+wlgh0794@users.noreply.github.com>
2024-06-12 19:30:22 +0900

    [level 0] Title: 두 수의 차, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
@@ -0,0 +1,17 @@
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+
+if __name__ == "__main__" :
+    
+    Data_list1 = [ 2 ,100 ]
+    Data_list2 = [ 100 , 2 ]
+    
+    
+    for i in Data_list1 :
+        
+        for k in Data_list1 :
+    
+            solution(  i  , k  )
+    
