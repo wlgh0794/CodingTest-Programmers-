8453ceafd3f48e5fd01c55926bf4075f60a7157e
wlgh0794 <167949216+wlgh0794@users.noreply.github.com>
2024-06-14 18:45:43 +0900

    [level 0] Title: 나이 출력, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.py
@@ -0,0 +1,15 @@
+def solution(age) :
+
+    answer = 2022 - age + 1
+
+    return answer
+
+
+if __name__ == "__main__" :
+    
+    
+    age_list = [ 40 , 23 ]
+    
+    for i in age_list :
+    
+        solution( i )
